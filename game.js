3b5287e1aa7f1b43dade3d463e41b24a166b18dc
adilkhr <adilkhair99@gmail.com>
2021-04-12 22:27:58 +0000

    Created a shuffle function to shuffle the cards on every page refresh

--- game.js
+++ game.js
@@ -15,3 +15,16 @@ let time;
 let minutes = 0;
 let seconds = 0;
 let timeStart = false;
+
+function shuffle(array) {
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
