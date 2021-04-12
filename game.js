9aa4774e2a0a6d61836b6d99a3911c884b32d04c
adilkhr <adilkhair99@gmail.com>
2021-04-12 22:05:11 +0000

    Created an empty array to store the opened cards and matched cards

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
 const deckCards = ["speedcola.png", "speedcola.png", "revive.png", "revive.png", "phd.png", "phd.png", "mulekick.png", "mulekick.png", "tombstone.png", "tombstone.png", "staminup.png", "staminup.png", "juggernog.png", "juggernog.png", "doubletap.png", "doubletap.png"];
 
 const deck = document.querySelector(".deck");
+let opened = [];
+let matched = [];
