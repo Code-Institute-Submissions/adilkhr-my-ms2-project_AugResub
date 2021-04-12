2083a0120f25b3cc6419432e40340e2b1954ca4a
adilkhr <adilkhair99@gmail.com>
2021-04-12 22:00:51 +0000

    Declared variable for deck to access the with class of .deck

--- game.js
+++ game.js
@@ -1,1 +1,3 @@
 const deckCards = ["speedcola.png", "speedcola.png", "revive.png", "revive.png", "phd.png", "phd.png", "mulekick.png", "mulekick.png", "tombstone.png", "tombstone.png", "staminup.png", "staminup.png", "juggernog.png", "juggernog.png", "doubletap.png", "doubletap.png"];
+
+const deck = document.querySelector(".deck");
