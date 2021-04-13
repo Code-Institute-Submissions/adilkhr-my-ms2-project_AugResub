da7ec5903186d4e7b0e52f4d19efc28f69d29d59
adilkhr <adilkhair99@gmail.com>
2021-04-13 12:30:04 +0000

    Created nomatch function if two cards dont match cards flip back over

--- game.js
+++ game.js
@@ -122,3 +122,14 @@ function match() {
     movesCounter();
     starRating();
 }
+
+function noMatch() {
+    setTimeout(function () {
+        opened[0].parentElement.classList.remove("flip");
+        opened[1].parentElement.classList.remove("flip");
+        document.body.style.pointerEvents = "auto";
+        opened = [];
+    }, 700);
+    movesCounter();
+    starRating();
+}
