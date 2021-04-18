a8ce0fef423a023b314b276226e05e06be48e6ab
adilkhr <adilkhair99@gmail.com>
2021-04-18 17:39:17 +0000

    changed name of first const variable

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const DECK_CARDS = ["speedcola.png", "speedcola.png", "revive.png", "revive.png", "phd.png", "phd.png", "mulekick.png", "mulekick.png", "tombstone.png", "tombstone.png", "staminup.png", "staminup.png", "juggernog.png", "juggernog.png", "doubletap.png", "doubletap.png"];
+const deckCards = ["speedcola.png", "speedcola.png", "revive.png", "revive.png", "phd.png", "phd.png", "mulekick.png", "mulekick.png", "tombstone.png", "tombstone.png", "staminup.png", "staminup.png", "juggernog.png", "juggernog.png", "doubletap.png", "doubletap.png"];
 
 const deck = document.querySelector(".deck");
 let opened = [];
